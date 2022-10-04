0d3400f371af2496a087b6bdac215183162c2c08
loopt-mkv <99065516+loopt-mkv@users.noreply.github.com>
2022-10-03 17:27:05 -0700

    id and conf is sent to onFileNameAssigned

--- src/node_trans_server.js
+++ src/node_trans_server.js
@@ -65,6 +65,7 @@ class NodeTransServer {
       conf.streamApp = app;
       conf.streamName = name;
       conf.args = args;
+      conf.id = id
       if (app === conf.app) {
         let session = new NodeTransSession(conf);
         this.transSessions.set(id, session);
--- src/node_trans_session.js
+++ src/node_trans_session.js
@@ -86,7 +86,7 @@ class NodeTransSession extends EventEmitter {
 
     console.log('this.conf', this.conf)
     if (this.conf['onFileNameAssigned']) {
-      this.conf['onFileNameAssigned'](filename)
+      this.conf['onFileNameAssigned'](this.conf)
     }
 
     this.ffmpeg_exec = spawn(this.conf.ffmpeg, argv);
